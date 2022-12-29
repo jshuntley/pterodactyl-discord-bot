fda88105f67fd0b1be34ad5630aca55dafa96d1c
jshuntley <joshuaehuntley@gmail.com>
2022-12-29 14:30:41 -0700

    modified:   .dockerignore 	modified:   .gitignore 	modified:   commands/tools/create-config-cmd.js 	deleted:    commands/tools/marco.js 	deleted:    components/buttons/create-config-btn.js 	deleted:    components/menus/channel-select.js 	modified:   functions/tools/onboarding.js

--- commands/tools/create-config-cmd.js
+++ commands/tools/create-config-cmd.js
@@ -3,7 +3,8 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("create-config")
-    .setDescription("Set up Pterodactyl server monitoring."),
+    .setDescription("Set up Pterodactyl server monitoring.")
+    .setDefaultMemberPermissions('0'),
     
     async execute(interaction, bot) {
       bot.createConfig(interaction);
--- functions/tools/onboarding.js
+++ functions/tools/onboarding.js
@@ -1,25 +1,13 @@
-const {
-  ButtonBuilder,
-  ButtonStyle,
-  ActionRowBuilder,
-} = require("discord.js");
+const { ButtonBuilder, ButtonStyle, ActionRowBuilder } = require("discord.js");
 
 module.exports = (bot) => {
   bot.onboarding = async () => {
-
-    const channel = bot.channels.cache.find(channel => channel.name === 'general');
-
-    const message = `This must be the first time you've run the bot.\nYou're going to need:\n• Pterodactyl Panel FQDN\n• Pterodactyl API Key\n• Pterodactyl Server ID\n• The Discord Channel ID\n Once you've got all that info, please click the button below to begin the setup process.`;
-
-    const button = new ActionRowBuilder().addComponents(
-      new ButtonBuilder()
-        .setCustomId("create-config")
-        .setLabel("Set Up")
-        .setStyle(ButtonStyle.Primary)
+    const channel = bot.channels.cache.find(
+      (channel) => channel.name === "general"
     );
 
-    await channel
-      .send({ content: message, components: [button] })
-      .catch(console.error);
+    const message = `This must be the first time you've run the bot.\nYou're going to need:\n• Pterodactyl Panel FQDN\n• Pterodactyl API Key\n• Pterodactyl Server ID\n• The Discord Channel ID\n Once you're ready, run \`/create-config\` to add your first server.`;
+
+    await channel.send({ content: message }).catch(console.error);
   };
 };
