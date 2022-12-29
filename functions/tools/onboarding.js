ee2a0c86a499b6a026f6b50962ac7fde96df5120
NineZulu <joshuaehuntley@gmail.com>
2022-12-29 15:05:10 -0700

    modified:   functions/tools/onboarding.js

--- functions/tools/onboarding.js
+++ functions/tools/onboarding.js
@@ -1,9 +1,7 @@
-const { ButtonBuilder, ButtonStyle, ActionRowBuilder } = require("discord.js");
-
 module.exports = (bot) => {
   bot.onboarding = async () => {
     const channel = bot.channels.cache.find(
-      (channel) => channel.name === "general"
+      (channel) => channel.name === "bot-commands"
     );
 
     const message = `This must be the first time you've run the bot.\nYou're going to need:\n• Pterodactyl Panel FQDN\n• Pterodactyl API Key\n• Pterodactyl Server ID\n• The Discord Channel ID\n Once you're ready, run \`/create-config\` to add your first server.`;
